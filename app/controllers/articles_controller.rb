7222ab36c9326f3fe8d2b69f950e98b88f68075f
Barna Biro <barna.biro@lateral-inc.com>
2020-04-22 16:02:15 +0300

    Article add - edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,41 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        
+    end
+
+    def create
+        @article = Article.new(article_params)
+        # render plain: @article.inspect
+        # byebug
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render "edit"
+        end
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
