33d5e5f294eb4d309851b80d664da89b8282beba
Barna Biro <barna.biro@lateral-inc.com>
2020-04-23 21:15:04 +0300

    Add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
     before_action :require_admin, except: [:index, :show]
+    before_action :set_category, only: [:edit, :update, :destroy]
     
     def index
         @categories = Category.paginate(page: params[:page], per_page: 3)
@@ -24,7 +25,24 @@ class CategoriesController < ApplicationController
         @category_articles = @category.articles.paginate(page: params[:page], per_page: 2)
     end
 
+    def edit
+        
+    end
+
+    def update
+        if @category.update(category_params)
+            flash[:notice] = "Category updated successfully"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        end
+    end
+
     private
+
+    def set_category
+        @category = Category.find(params[:id])
+    end
 
     def category_params
         params.require(:category).permit(:name)
