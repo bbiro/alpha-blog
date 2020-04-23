e619975c722e28013bfb1e9798b01850d1d0adb7
Barna Biro <barna.biro@lateral-inc.com>
2020-04-23 15:21:38 +0300

    update logged user to be the article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = helpers.current_user
         # render plain: @article.inspect
         # byebug
         if @article.save
