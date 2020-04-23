c2877617f6d81b5b5e0cdedc4d04d6c8f3e50d0e
Barna Biro <barna.biro@lateral-inc.com>
2020-04-23 11:32:39 +0300

    Add edit template and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,40 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  before_action :set_user, only: [:edit, :update, :destroy]  
+  def new
+      @user = User.new
 
+  end
+
+  def create
+      @user = User.new(user_params)
+      if (@user.save)
+          flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully sign up."
+          redirect_to articles_path
+      else
+          render 'new'
+      end
+  end
+
+  def edit
+  end
+
+  def update
+    byebug
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        if (@user.save)
-            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully sign up."
-            redirect_to articles_path
-        else
-            render 'new'
-        end
-    end
+  private
 
-    private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+      params.require(:user).permit(:username, :email, :password)
+  end
 end
