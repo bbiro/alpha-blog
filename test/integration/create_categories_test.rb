89b68c6e1814eae49566cbb3efe71d18fa8ee7bd
Barna Biro <barna.biro@lateral-inc.com>
2020-04-23 20:07:39 +0300

    Many to Many Associations

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
     
+    def setup
+        @user = User.create(username: "johny", email: "john@example.com", password: "password", admin: true)
+    end
+
     test "get new category form and create category" do
+        sign_in_as(@user, "password")
         get new_category_path
         assert_template 'categories/new'
         # test create
@@ -17,6 +22,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
 
     test "invalid category submission results in failure" do
+        sign_in_as(@user, "password")
         get new_category_path
         assert_template 'categories/new'
         # test create, but should not be difference - error is happening
